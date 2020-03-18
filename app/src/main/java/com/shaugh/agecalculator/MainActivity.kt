f16bb3871ca2b14a34f2c6f30a395096166f6ede
SharifulIsalmShaugh <13303060@iubat.edu>
2020-03-18 17:21:11 +0600

    2nd

--- app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
+++ app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
@@ -4,21 +4,30 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+import java.time.Month
+import java.time.Period
 import java.util.*
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+abstract class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var birthDate: LocalDate
+    lateinit var todayDate: LocalDate
+    lateinit var yourAge: Period
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         etToday.setOnClickListener(this)
         etBday.setOnClickListener(this)
+       /* if ((yourAge != null )){
+            ageCalculate()
+        }*/
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -26,24 +35,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (v == etToday) {
             // Get Current Date
             val c: Calendar = Calendar.getInstance()
-            var mYear = c.get(Calendar.YEAR)
-            var mMonth = c.get(Calendar.MONTH)
-            var mDay = c.get(Calendar.DAY_OF_MONTH)
+            val cYear = c.get(Calendar.YEAR)
+            val cMonth = c.get(Calendar.MONTH)
+            val cDay = c.get(Calendar.DAY_OF_MONTH)
 
             val datePickerDialog = DatePickerDialog(
                 this,
                 OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     etToday.text = dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
-                }, mYear, mMonth, mDay
+                }, cYear, cMonth, cDay
             )
+            todaysDate(cYear, cMonth, cDay)
             datePickerDialog.show()
         }
         if (v == etBday) {
             // Get Birth Date
             val c: Calendar = Calendar.getInstance()
-            var mYear = c.get(Calendar.YEAR)
-            var mMonth = c.get(Calendar.MONTH)
-            var mDay = c.get(Calendar.DAY_OF_MONTH)
+            val mYear = c.get(Calendar.YEAR)
+            val mMonth = c.get(Calendar.MONTH)
+            val mDay = c.get(Calendar.DAY_OF_MONTH)
 
             val datePickerDialog = DatePickerDialog(
                 this,
@@ -51,7 +61,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     etBday.text = dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
                 }, mYear, mMonth, mDay
             )
+            bdaysDate(mYear, mMonth, mDay)
             datePickerDialog.show()
         }
+    }
+
+     fun todaysDate(year: Int, month: Int, day: Int){
+         todayDate = LocalDate.of(year, Month.of(month), day)
+     }
+     fun bdaysDate(year: Int, month: Int, day: Int){
+         birthDate = LocalDate.of(year, Month.of(month), day)
+     }
+    fun ageCalculate() {
+
+        val yourAge: Period = Period.between(birthDate, todayDate)
+        Log.e("YOU", "Year " + yourAge.getYears().toString() + " - Month " + yourAge.getMonths().toString() + " - Day " + yourAge.getDays().toString())
+
     }
 }
