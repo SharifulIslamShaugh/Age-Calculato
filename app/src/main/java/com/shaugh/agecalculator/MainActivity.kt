5065799c940e9e169ed5621539e68d867dd96d0b
SharifulIsalmShaugh <13303060@iubat.edu>
2020-03-20 13:41:37 +0600

    4th

--- app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
+++ app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
@@ -4,12 +4,9 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
@@ -19,8 +16,6 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    var birth = 0
-    var today = 0
     lateinit var birthDate: LocalDate
     lateinit var todayDate: LocalDate
     lateinit var yourAge: Period
@@ -30,7 +25,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         etToday.setOnClickListener(this)
         etBday.setOnClickListener(this)
+        btnCalculate.setOnClickListener(this)
+        btnClear.setOnClickListener(this)
+
+        btnCalculate.isClickable = false
+        btnClear.isClickable = false
     }
+
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
@@ -51,8 +52,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 this,
                 OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     todaysDate(year, monthOfYear, dayOfMonth)
-                    etToday.text = dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
-                    today = 1
+                    etToday.text =
+                        " " + dayOfMonth.toString() + " - " + (monthOfYear + 1) + " - " + year
+
                 }, cYear, cMonth, cDay
             )
 
@@ -69,35 +71,62 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 this,
                 OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     bdaysDate(year, monthOfYear, dayOfMonth)
-                    etBday.text = dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
-                    birth = 1
+                    etBday.text =
+                        " " + dayOfMonth.toString() + " - " + (monthOfYear + 1) + " - " + year
+                    btnCalculate.isClickable = true
+                    btnClear.isClickable = true
                 }, mYear, mMonth, mDay
             )
             datePickerDialog.show()
         }
-        if (today != 0 && birth !=0){
-            btnCalculate.isClickable = true
-            if (v == btnCalculate){
-                yourAge = Period.between(birthDate, todayDate)
-                Log.e("YOU", "Year " + yourAge.getYears().toString() + " - Month " + yourAge.getMonths().toString() + " - Day " + yourAge.getDays().toString())
+
+        if (v == btnCalculate) {
+            ivQuestion.clearAnimation()
+            ivQuestion.visibility = View.GONE
+            yourAge = Period.between(birthDate, todayDate)
+            tvAgeYear.text = yourAge.years.toString()
+            tvAgeMonth.text = yourAge.months.toString()
+            tvAgeDay.text = yourAge.days.toString()
+            when {
+                yourAge.years < 21 -> {
+                    ivAgeWise.setImageResource(R.drawable.little)
+                    animation = AnimationUtils.loadAnimation(this, R.anim.bounce_anim)
+                    ivAgeWise.startAnimation(animation)
+                }
+                yourAge.years in 21..40 -> {
+                    ivAgeWise.setImageResource(R.drawable.young)
+                    animation = AnimationUtils.loadAnimation(this, R.anim.bounce_anim)
+                    ivAgeWise.startAnimation(animation)
+                }
+                else -> {
+                    ivAgeWise.setImageResource(R.drawable.old)
+                    animation = AnimationUtils.loadAnimation(this, R.anim.bounce_anim)
+                    ivAgeWise.startAnimation(animation)
+                }
             }
         }
-        if (today != 0 && birth !=0){
-            btnCalculate.isClickable = true
-            if (v == btnClear){
-                today = 0
-                birth = 0
-                etToday.text = ""
-                tvBday.text = ""
-            }
+
+        if (v == btnClear) {
+            tvAgeYear.text = 0.toString()
+            tvAgeMonth.text = 0.toString()
+            tvAgeDay.text = 0.toString()
+            etToday.text = ""
+            etBday.text = ""
+
+            ivQuestion.visibility = View.VISIBLE
+            ivAgeWise.setImageResource(R.drawable.mr)
+            animation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim)
+            ivQuestion.startAnimation(animation)
+
         }
     }
 
-     fun todaysDate(year: Int, month: Int, day: Int){
-         todayDate = LocalDate.of(year, Month.of(month), day)
-     }
-     fun bdaysDate(year: Int, month: Int, day: Int){
-         birthDate = LocalDate.of(year, Month.of(month), day)
-     }
+    private fun todaysDate(year: Int, month: Int, day: Int) {
+        todayDate = LocalDate.of(year, Month.of(month), day)
+    }
+
+    private fun bdaysDate(year: Int, month: Int, day: Int) {
+        birthDate = LocalDate.of(year, Month.of(month), day)
+    }
 
 }
