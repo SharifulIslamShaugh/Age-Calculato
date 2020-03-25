6e121f16566ba8c2f16126c40dca90ea28233d53
SHAUGH <shaugh@raiseit-bd.com>
2020-03-25 10:46:49 -0700

    with total days, months and years

--- app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
+++ app/src/main/java/com/shaugh/agecalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -12,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 import java.time.Month
 import java.time.Period
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var todayDate: LocalDate
     lateinit var yourAge: Period
     lateinit var animation: Animation
+    lateinit var animation2: Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,9 +30,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         etBday.setOnClickListener(this)
         btnCalculate.setOnClickListener(this)
         btnClear.setOnClickListener(this)
+        btnMore.setOnClickListener(this)
+        btnLess.setOnClickListener(this)
 
         btnCalculate.isClickable = false
         btnClear.isClickable = false
+
     }
 
 
@@ -51,7 +57,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val datePickerDialog = DatePickerDialog(
                 this,
                 OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                    todaysDate(year, monthOfYear, dayOfMonth)
+                    todaysDate(year, monthOfYear+1, dayOfMonth)
                     etToday.text =
                         " " + dayOfMonth.toString() + " - " + (monthOfYear + 1) + " - " + year
 
@@ -70,7 +76,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val datePickerDialog = DatePickerDialog(
                 this,
                 OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                    bdaysDate(year, monthOfYear, dayOfMonth)
+                    bdaysDate(year, monthOfYear+1, dayOfMonth)
                     etBday.text =
                         " " + dayOfMonth.toString() + " - " + (monthOfYear + 1) + " - " + year
                     btnCalculate.isClickable = true
@@ -81,6 +87,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         if (v == btnCalculate) {
+            btnMore.visibility = View.VISIBLE
             ivQuestion.clearAnimation()
             ivQuestion.visibility = View.INVISIBLE
             yourAge = Period.between(birthDate, todayDate)
@@ -88,7 +95,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             tvAgeMonth.text = yourAge.months.toString()
             tvAgeDay.text = yourAge.days.toString()
             when {
-                yourAge.years < 0 -> {
+                yourAge.days < 0 || yourAge.months < 0 || yourAge.years < 0 -> {
                     ivAgeWise.setImageResource(R.drawable.negetive)
                     animation = AnimationUtils.loadAnimation(this, R.anim.bounce_anim)
                     ivAgeWise.startAnimation(animation)
@@ -117,12 +124,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             tvAgeDay.text = 0.toString()
             etToday.text = ""
             etBday.text = ""
-
+            lln5.visibility = View.GONE
+            tvTotalYear.text = 0.toString()
+            tvTotalMonth.text = 0.toString()
+            tvTotalDay.text = 0.toString()
             ivQuestion.visibility = View.VISIBLE
+            btnMore.visibility = View.GONE
             ivAgeWise.setImageResource(R.drawable.mr)
             animation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim)
             ivQuestion.startAnimation(animation)
 
+        }
+        if (v == btnMore){
+            ivQuestion.visibility = View.GONE
+            btnLess.visibility = View.VISIBLE
+            lln5.visibility = View.VISIBLE
+            ivAgeWise.visibility = View.GONE
+            btnMore.visibility = View.GONE
+            val years: Long = ChronoUnit.YEARS.between(birthDate, todayDate)
+            val months: Long = ChronoUnit.MONTHS.between(birthDate, todayDate)
+            val days: Long = ChronoUnit.DAYS.between(birthDate, todayDate)
+
+            tvTotalYear.text = years.toString()
+            tvTotalMonth.text = months.toString()
+            tvTotalDay.text = days.toString()
+            animation = AnimationUtils.loadAnimation(this, R.anim.move_anim)
+            lln5.startAnimation(animation)
+        }
+        if (v == btnLess){
+            animation = AnimationUtils.loadAnimation(this, R.anim.slid_down_anim)
+            animation2 = AnimationUtils.loadAnimation(this, R.anim.bounce_anim)
+            btnMore.visibility = View.VISIBLE
+            btnLess.visibility = View.GONE
+            lln5.visibility = View.GONE
+            lln5.startAnimation(animation)
+            ivQuestion.visibility = View.INVISIBLE
+            ivAgeWise.visibility = View.VISIBLE
+            ivAgeWise.startAnimation(animation2)
         }
     }
 
